5eb5b6115aea858690dbc000b214fdd694c7435d
VicDoom <55551616+VicDoom@users.noreply.github.com>
2020-07-04 22:39:06 +0300

    added forms handler

--- js/script.js
+++ js/script.js
@@ -46,8 +46,8 @@ window.addEventListener('DOMContentLoaded', function(){
             'total' : t,
             'hours' : hours,
             'minutes' : minutes,
-            'seconds' : seconds
-        }
+            'seconds' : seconds 
+        };
     }
 
     function setClock(id, endtime) {
@@ -107,6 +107,49 @@ window.addEventListener('DOMContentLoaded', function(){
         overlay.style.display = 'none';
         //more.classList.remove('more-splash');
         document.body.style.overflow = '';
+    });
+
+    //Form
+
+    let message = {
+        loading: "Загрузка",
+        success: "Ваша заявка принята, скоро вам перезвонят",
+        failure: "Ошибка :("
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.querySelector('input');
+
+    let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', "server.php");
+        request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+        let formData = new FormData(form);
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState == 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
     });
 
 });
